6c83509058d4cb34caecf9901dd8e4646dbb2577
David <david@gotrunks.es>
2017-07-27 19:31:16 +0200

    force start and finish on regex check

--- punsbot.py
+++ punsbot.py
@@ -70,7 +70,7 @@ def findPun(message="",dbfile='puns.db'):
         triggers = cursor.execute('''SELECT trigger from puns where chatid = ? or chatid = 0 order by chatid desc''',(message.chat.id,)).fetchall()
         for i in triggers:
             if isValidRegex(i[0]):
-                regexp = re.compile(i[0])
+                regexp = re.compile('^' + i[0] + '$')
                 if regexp.match(last[-1]) != None:
                     answer = cursor.execute('''SELECT pun from puns where trigger = ? AND (chatid = ? OR chatid = 0) ORDER BY chatid desc''', (i[0], message.chat.id)).fetchone()
                     db.commit()
